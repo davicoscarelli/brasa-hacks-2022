e1f0cb682b565507033b37515bf5c15413a7d509
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2022-04-04 10:34:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,20 @@ app = Flask(__name__)
 def serve_model():
   try:
     print("entrou")
-    request_data = request.get_json(force=True)
-    img = request_data['img']
+    # request_data = request.get_json(force=True)
+    # img = request_data['img']
+    file = request.files['image'].read()
     # img = np.array(img).reshape(-1, 224, 224, 3)
     # print("reshape")
     # x = tf.keras.preprocessing.image.img_to_array(img)
     # x = np.expand_dims(x, axis=0)
     # x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
+    img = tf.keras.preprocessing.image.load_img(file, target_size=(224, 224))
+    x = tf.keras.preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
     print("rodou x")
-    preds = model.predict(img)
+    preds = model.predict(x)
     print("predicted")
     label_map = {'dangerously deep': 0,
     'feet-dont-touch deep': 1,
